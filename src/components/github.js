4973fea70562ac156d689f1e0a883886b175e3b5
ranganathnk <ranganathnkulkarni@gmail.com>
2022-12-11 12:32:16 +0530

    updated ui

--- src/components/github.js
+++ src/components/github.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBInputGroup } from "mdb-react-ui-kit";
+import { MDBBtn } from "mdb-react-ui-kit";
 
-const Github = () => {
+export default function Github() {
   return (
-    <div>github</div>
-  )
+    <div className="container">
+      <br />
+      <br />
+      <div className="card">
+        <div className="card-header">GitHub Repositories</div>
+        <div className="card-body">
+          <h5 className="card-title">Enter GitHub Username</h5>
+          <div className="md-form">
+            <div class="input-group mb-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text">https://github.com/</div>
+              </div>
+              <input
+                type="text"
+                class="form-control py-0"
+                id="inlineFormInputGroup"
+                placeholder="Enter Username"
+              />
+            </div>
+          </div>
+          <br />
+          <a href="#!" className="btn btn-primary">
+            Fetch Repos
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Github
